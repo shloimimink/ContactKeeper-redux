a6d085bb8017cbbe6e7b78dc28668f232e7d0dcc
shloimi minkowicz <shloimimink@gmail.com>
2019-07-25 22:58:27 -0500

    got-started-on-the-frontend

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -29,19 +29,30 @@ const ContactState = props => {
                 phone: '333-333-3333',
                 type: 'professional'
             },
-        ]
-    }
+        ],
+        current: null
+    };
 
     const [state, dispatch] = useReducer(contactReducer, initialState);
 
     // Add Contact
+    const addContact = (contact) => {
+      contact.id = uuid.v4();
+      dispatch({type: ADD_CONTACT, payload: contact})
+    };
 
     // Delete Contact
-
+    const deleteContact = (id) => {
+        dispatch({type: DELETE_CONTACT, payload: id});
+    };
     // Set Current Contact
-
+    const setCurrent = (contact) => {
+        dispatch({type: SET_CURRENT, payload: contact});
+    };
     // Clear Current Contact
-
+    const clearCurrent = () => {
+        dispatch({type: CLEAR_CURRENT});
+    };
     // Update Contact
 
     // Filter Contacts
@@ -51,7 +62,12 @@ const ContactState = props => {
     return (
         <ContactContext.Provider
         value={{
-            contacts: state.contacts
+            contacts: state.contacts,
+            current: state.current,
+            addContact,
+            deleteContact,
+            setCurrent,
+            clearCurrent
         }}
         >
             {props.children}
